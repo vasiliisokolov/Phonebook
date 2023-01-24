b4244c48d66c59c4a9ae4becd23a49a26106f7bd
VasiliiSokolov <vasilysokolov@mail.ru>
2023-01-24 20:36:18 +0300

    2/3 functions

--- Phonebook.cpp
+++ Phonebook.cpp
@@ -11,6 +11,9 @@ enum command
 
 };
 
+void insert_data(std::map<std::string, std::string>&);
+std::string find_number(std::map<std::string, std::string>&);
+
 int main()
 {
     int operation;
@@ -20,15 +23,44 @@ int main()
     std::cout << "To add the abonent - press 1" << std::endl;
     std::cout << "To find abonent's surname - press 2" << std::endl;
     std::cout << "To find abonent' telefonnumber - press 3" << std::endl;
+    std::cin >> operation;
 
     switch (operation)
     {
     case command::add:
+        insert_data(phoneBook);
         break;
     case command::find_surname:
+        std::cout << find_number(phoneBook) << std::endl;
         break;
     case command::find_telNumber:
         break;
     }
 }
 
+void insert_data(std::map<std::string, std::string>& phoneBook)
+{
+    std::pair<std::string, std::string> insert;
+    std::cout << "Enter the telephone number: ";
+    std::cin >> insert.first;
+    std::cout << "Enter the surname: ";
+    std::cin >> insert.second;
+    phoneBook.insert(insert);
+}
+
+std::string find_number(std::map<std::string, std::string>& phoneBook)
+{
+    std::string insert;
+    std::cout << "Enter the telephone number: ";
+    std::cin >> insert;
+    std::map<std::string, std::string> ::iterator it;
+    it = phoneBook.find(insert);
+    if (it == phoneBook.end())
+    {
+        return "Abonent is not find!";
+    }
+    else
+    {
+        return it->second;
+    }
+}
